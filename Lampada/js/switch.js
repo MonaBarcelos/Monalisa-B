440fe4a4b7687df5714f74ee863d013eab50817b
MonaBarcelos <monaabarcelos@gmail.com>
2024-11-06 21:06:51 -0300

    Atualização e Finalização do Projeto

--- Lampada/js/switch.js
+++ Lampada/js/switch.js
@@ -1,3 +1,9 @@
 function turnlight() {
-    
+    //Lampada
+    const bulb = document.querySelector(".bulb");
+    //Interruptor
+    const buttom = document.querySelector(".switch");
+
+    buttom.classList.toggle("on");
+    bulb.classList.toggle("on");
 }
